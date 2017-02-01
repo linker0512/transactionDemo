e0fd0556c5c033392cb184b435428af32fded700
zj <zhangjicoding@gmail.com>
2017-02-01 12:25:29 +0800

    callable实现线程回掉，不能知道js脚本什么时候结束 更改使用包装js脚本特定字符串

--- src/main/java/tool/Cmd.java
+++ src/main/java/tool/Cmd.java
@@ -2,6 +2,7 @@ package tool;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.concurrent.*;
 
 /**
  * Created by zj on 2017-1-31.
@@ -13,7 +14,7 @@ public class Cmd {
     private ProcessBuilder processBuilder;
     private Process process;
     private ArrayList<String> accounts;
-    public Cmd(){
+    public Cmd() {
         processBuilder = new ProcessBuilder("geth.exe", "attach");
         processBuilder.redirectErrorStream(true);
         accounts = new ArrayList<String>();
@@ -24,28 +25,30 @@ public class Cmd {
         }
         reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+
         LoadScript();
-        ReadCmd();
-
-       // GetAccounts();
+        GetAccounts();
     }
-    private void ReadCmd() {
-       synchronized (this){
-           new Thread(() ->{
-               try {
-                    for (String line ; ((line = reader.readLine()) != null);){
-                        System.out.println(line);
-                        }
-               } catch (IOException e) {
-                   e.printStackTrace();
-               }
-               //System.out.println("111111111");
-           }).start();
-       }
+    private String ReadCmd() {
+        final ExecutorService service;
+        final Future<String> task;
+        service = Executors.newFixedThreadPool(1);
+        task = service.submit(new ReadCallBack());
+        try {
+            final String result = task.get();
+            service.shutdownNow();
+            return result;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
-    public void WriteCmd(String command){
-        synchronized (this) {
+    public String WriteCmd(String command){
             try {
                 writer.write(command);
                 writer.newLine();
@@ -53,27 +56,24 @@ public class Cmd {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        return ReadCmd();
+//        Thread writecommand =  new Thread(new WriteThread(command));
+//        writecommand.start();
+//        try {
+//            writecommand.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        //System.out.println("111111111111111");
+
     }
     public void LoadScript(){
         WriteCmd("loadScript(\"src/main/resources/getblock.js\")");
     }
 
     private ArrayList<String> GetAccounts() {
-        //ArrayList<String> _accounts = new ArrayList<String>();
-        try {
-            writer.write("getAccounts()");
-            writer.newLine();
-            writer.flush();
-            for (String line ; ((line = reader.readLine()) != null)
-                    && (line.length() != 0); ){
-                if(!line.equals("> ") && (!line.equals("undefined")))
-                    accounts.add(line);
-                //System.out.println(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //String result = WriteCmd("eth.accounts");
+        //result.chars().
         return accounts;
     }
 
@@ -87,4 +87,53 @@ public class Cmd {
         accounts.add("123");
         System.out.println("personal.unlockAccount("+accounts.get(0)+","+"ethzhangji"+", 300)");
     }
+
+    private class ReadCallBack implements Callable<String> {
+        public String result;
+        private char[] buf;
+        public ReadCallBack(){buf = new char[1024];}
+        public String call() {
+            try {
+
+                Thread.sleep(5000);
+                int len = 1;
+                len = reader.read(buf);
+                result = new String(buf,0,len);
+                System.out.println(result);
+            } catch(final InterruptedException ex) {
+                ex.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return (result);
+        }
+    }
+
+    private class WriteThread implements Runnable{
+
+        private String command;
+        WriteThread(String a){
+            command = a;
+        }
+        @Override
+        public void run() {
+            try {
+                writer.write(command);
+                writer.newLine();
+                writer.flush();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+//    private class ReadThread implements Runnable{
+//
+//        @Override
+//        public void run() {
+//
+//        }
+//    }
 }
